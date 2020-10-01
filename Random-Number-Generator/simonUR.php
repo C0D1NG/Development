75a957ea3e172cf80191942d1d6bc89dae2e5f66
SimonUR <simon-skater@gmx.de>
2020-10-01 22:19:38 +0200

    added random number generator in PHP

---
+++ Random-Number-Generator/simonUR.php
@@ -0,0 +1,13 @@
+<?php 
+      
+    // Generating a random number 
+    $randomNumber = rand(); 
+      
+    // Print 
+    print_r($randomNumber); 
+      
+    // Generating a random number in a given range. 
+    $randomNumber = rand(15,35); 
+      
+    // Print 
+    print_r($randomNumber); 
