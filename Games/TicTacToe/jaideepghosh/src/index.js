f0fbefb0cfefaa1d42d458efeafb02cc16cc634c
jaideepghosh <er.jaideepghosh@gmail.com>
2020-10-02 00:52:45 +0530

    feat(app):Added index template.

---
+++ Games/TicTacToe/jaideepghosh/src/index.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function Game() {
+  return <div>Hello World</div>;
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
