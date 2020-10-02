7e0317b37dff0d4dfcb8234d637f2749217530f6
G1Joshi <joshijeevan98@gmail.com>
2020-10-02 15:15:29 +0530

    organized

---
+++ Calculator/Gajerachintan/design.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll('button');
+// select the <input type="text" class="display" disabled> element
+const display = document.querySelector('.display');
+
+// add eventListener to each button
+buttons.forEach(function(button) {
+  button.addEventListener('click', calculate);
+});
+
+// calculate function
+function calculate(event) {
+  // current clicked buttons value
+  const clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === '=') {
+    // check if the display is not empty then only do the calculation
+    if (display.value !== '') {
+      // calculate and show the answer to display
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === 'C') {
+    // clear everything on display
+    display.value = '';
+  } else {
+    // otherwise concatenate it to the display
+    display.value += clickedButtonValue;
+  }
+}
