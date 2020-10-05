e4c0f5cbe8136e00b023e1c360dcd9e6d702c625
hkishawi <hkishawi@gmail.com>
2020-10-05 14:22:11 -0400

    minor edits

--- hkishawi/script.js
+++ hkishawi/script.js
@@ -1,6 +1,5 @@
 
 function RandomInt () {
-    var rnd = Math.floor(Math.random() * 10000) + 1;
-    return ("Yay! Your number is: " + rnd)
-  }
-  
+  var rnd = Math.floor(Math.random() * 10000) + 1
+  return ('Yay! Your number is: ' + rnd)
+}
