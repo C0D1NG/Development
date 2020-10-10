4aba3cec3b1fb391c6e990726a1ef250eacd79f3
G1Joshi <joshijeevan98@gmail.com>
2020-10-10 07:35:29 +0530

    organized

--- SeverX/Two Sums/task.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
-
-print(Solution().twoSum([2,7,11,15], 9))
